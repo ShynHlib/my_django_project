398e33a0d62b6f6964ffcde1999fcb1dcc8f1ee6
Hlib Shyn <gshin907@gmail.com>
2025-04-19 17:38:25 +0200

    User management: initial email verification

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -137,3 +137,6 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # change later to smtp
