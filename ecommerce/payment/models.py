085632b434d66ea1afffa08ef4e7941284d032bd
Hlib Shyn <gshin907@gmail.com>
2025-05-08 10:12:22 +0200

    Integrate order functionality

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 class ShippingAddress(models.Model):
     full_name = models.CharField(max_length=200)
@@ -20,3 +21,27 @@ class ShippingAddress(models.Model):
         return 'Shipping Address: ' + str(self.id)
 
 
+class Order(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=300)
+    shipping_address = models.TextField(max_length=1000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order - #' + str(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order item - #' + str(self.id)
